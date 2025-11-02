ca7905b41d5f4cc2b34b5eb5de38f98da92261fe
vanessa <vane.busato9@gmail.com>
2025-11-02 12:45:39 +0100

    aggiunto es.5 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -28,4 +28,5 @@ const isFabioPresent = teachers.includes("Fabio");
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(",");
+console.log(teachersString);
