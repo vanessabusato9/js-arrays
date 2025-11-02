2bd2c94abd26516fb6631b10bfe27815abb13320
vanessa <vane.busato9@gmail.com>
2025-11-02 11:25:16 +0100

    aggiunto esercizio 2 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,7 +16,8 @@ console.log(teachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter( teacher => teacher.length >= 5 );
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
