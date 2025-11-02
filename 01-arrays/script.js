5d4442c1efed3d7a2e8dd1e1d8513e9f396faf7c
vanessa <vane.busato9@gmail.com>
2025-11-02 10:44:00 +0100

    aggiunto es.5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,6 +29,8 @@ console.log(teachers);
 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
