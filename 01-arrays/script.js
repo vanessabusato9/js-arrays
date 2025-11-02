39675392740c02a67005ca6155b91c56527adbc8
vanessa <vane.busato9@gmail.com>
2025-11-02 10:59:14 +0100

    aggiunto es.8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -43,4 +43,6 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length == 0 ;
+console.log(isTeachersEmpty);
+
